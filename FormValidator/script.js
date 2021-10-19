281611c58a742bfbec06166b10245fd73bee7cca
Meredith Jones <mj19003@gmail.com>
2021-10-19 19:42:21 -0400

    Added event listener to form, with preventDefault, successfully logging username.value

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -4,3 +4,9 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    console.log(username.value);
+});
