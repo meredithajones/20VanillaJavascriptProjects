042157e7c90521c4aedb0e441d422457704c9b93
Meredith Jones <mj19003@gmail.com>
2021-10-20 16:20:04 -0400

    Setting up display of error messaging on the DOM

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -5,10 +5,21 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show input error message
+function showError(input, message) {
+    //accessing class of form-control through input.parentElement
+    const formControl = input.parentElement;
+}
+
+
+// Event Listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     if (username.value === '') {
         showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
     }
 });
