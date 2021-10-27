0bb4911bfe1a925ca1c5dc2cfbc3b161f2e59041
Meredith Jones <mj19003@gmail.com>
2021-10-27 19:07:48 -0400

    Refactoring validateEmail function

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -23,9 +23,13 @@ function showSuccess(input) {
 }
 
 // Check if email is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if() {
+
+    } else {
+
+    }
 }
 
 //Check required fields
